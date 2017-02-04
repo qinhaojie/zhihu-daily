dae9ff53f4328d976a6bddc2eea7c381754f9f31
qinhaojie <qinhaojie1992@163.com>
2017-02-04 18:54:03 +0800

    bug

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,10 +3,9 @@ import thunkMiddleware from 'redux-thunk'
 import rootRuducer from './reducers'
 import filterLinkMiddleware from './filterLinkMiddleware'
 const finalCreateStore = compose(
-  applyMiddleware(thunkMiddleware, filterLinkMiddleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunkMiddleware, filterLinkMiddleware)
 )(createStore)
 
 export default function(state) {
-  return finalCreateStore(rootRuducer, state)
+  return finalCreateStore(rootRuducer, state, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 }
