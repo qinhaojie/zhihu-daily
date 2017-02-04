d0bf32893688c54610e55ad5075115ab21cfc476
qinhaojie <qinhaojie1992@163.com>
2017-02-04 14:43:24 +0800

    redux dev tool

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,8 @@ import thunkMiddleware from 'redux-thunk'
 import rootRuducer from './reducers'
 
 const finalCreateStore = compose(
-  applyMiddleware(thunkMiddleware)
+  applyMiddleware(thunkMiddleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )(createStore)
 
 export default function(state) {
