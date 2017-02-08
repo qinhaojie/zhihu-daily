603e8010a0a28417503d61f55a743aba296d7501
qinhaojie <qinhaojie1992@163.com>
2017-02-08 19:47:25 +0800

    代理api 的301问题

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
 var proxy = require('express-http-proxy')
-
+var http = require('http')
 var publicPath = './build/'
+var request = require('request')
 app.use(express.static(publicPath))
 app.use('/proxyimg', proxy('pic1.zhimg.com', {
   decorateRequest: function(proxyReq, originalReq) {
@@ -13,16 +14,21 @@ app.use('/proxyimg', proxy('pic1.zhimg.com', {
     return proxyReq
   }
 }))
+app.use('/zapi', function(req, res){
+  var url = 'http://news-at.zhihu.com' + req.url
+  request(
+    {
+      url: url,
+      json: true
+    }, (err, response, data) => {
+    if (!err && response.statusCode === 200) {
+      res.send(data)
+    } else {
+      res.send(JSON.stringify(response))
+    }
+  })
+})
 
-app.use('/zapi', proxy('news-at.zhihu.com', {
-  // decorateRequest: function(proxyReq, originalReq) {
-  //   delete proxyReq.headers.referer
-  //   console.log(originalReq)
-  //   // proxyReq.url = originalReq.originalUrl
-  //   console.log(proxyReq.url)
-  //   return proxyReq
-  // }
-}))
 app.listen(8080,function(e){
   if(e) throw e
   console.log('run at 8080')
