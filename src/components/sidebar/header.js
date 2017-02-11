df37ec3d24c082c57c563104e77830bcfbaa65c0
qinhaojie <qinhaojie1992@163.com>
2017-02-11 11:36:46 +0800

    sidebar header

--- src/components/sidebar/header.js
+++ src/components/sidebar/header.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
 import './header.less'
-
+import img from './avatar.jpg'
 export default class Header extends Component {
   render() {
     return (
       <div className="sidebar-header">
-        
+        <header>
+          <img src={img} alt="" className="avatar"/>
+          <span>请登录</span>
+        </header>
       </div>
     )
   }
